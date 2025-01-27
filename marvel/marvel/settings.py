32f9592e93a265dc6c9829adceaa82bd29a6ed92
Subha Rajagopal <73748056+Subha822@users.noreply.github.com>
2025-01-27 09:01:32 +0000

    Code Update

--- marvel/marvel/settings.py
+++ marvel/marvel/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-cv*i$ibdzi6cpm@m&5+_6o@aow)9i*xjq+^k-*uf35_@t$*p0w"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
